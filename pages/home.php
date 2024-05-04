ed2574e17e2db89a1c7d005fed8718defd206a76
BilalMaryam <142595592+BilalMaryam@users.noreply.github.com>
2024-05-04 15:14:54 +0300

    adding a Home Page

---
+++ pages/home.php
@@ -0,0 +1,141 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body class="my-5 mx-5">
+    <section class="hero">
+        <div class="row">
+            <div class="container col-6">
+                <h5>Medical Checkup</h5>
+                <br>
+                <h1 class="fw-bold">Best Medical Clinic For Your Healthcare</h1>
+                <p>We will take care of your health and make sure you get the best service.
+                    All is needed is booking an appointment to Begin a journey on defeating the sickness,
+                    then leave the rest to us.</p>
+                <button type="button" class="btn btn-primary">Book An appointment</button>
+            </div>
+
+            <div class="col-2"></div>
+
+            <div class="pic-container col-4">
+                <img style="border-radius: 5px; height: 320px; width: 400px;"
+                    src="https://tse1.mm.bing.net/th?id=OIP.E-ZeeuHCWJi1Rlym1Dn5GQHaFj&pid=Api&P=0&h=220">
+            </div>
+        </div>
+    </section>
+
+    <div>
+        <div class="text-center">
+            <h5>MEDCAL CHECKUP</h5>
+            <h1>
+                <span>
+                    Why Choose Gomedic For
+                    Your Healthcare?
+                </span>
+            </h1>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-3 border border-primary pt-3" style="text-align: center; border-radius: 10px;">
+                <svg class="bi bi-person border rounded-circle d-inline-flex justify-content-center align-items-center px-1 py-1"
+                    xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" viewBox="0 0 16 16"
+                    style="background-color: rgb(188, 227, 226);">
+                    <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                </svg>
+                <h3>All Specialist</h3>
+                <p>We have some of the best doctors and employees in this Country.</p>
+            </div>
+
+            <div class="col-1"></div>
+
+            <div class="col-3 border border-primary pt-3"
+                style="text-align: center; border-radius: 10px; border-color: blue;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                    class="bi bi-check2 border rounded-circle d-inline-flex justify-content-center align-items-center px-1 py-1"
+                    viewBox="0 0 16 16" style="background-color: rgb(188, 227, 226);">
+                    <path
+                        d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0" />
+                </svg>
+                <h3>Medical Record</h3>
+                <p>you can have access to your Medical Records to help on your recovery.</p>
+            </div>
+
+            <div class="col-1"></div>
+
+            <div class="col-4 border border-primary pt-3" style="text-align: center; border-radius: 10px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                    class="bi bi-headphones border rounded-circle d-inline-flex justify-content-center align-items-center px-1 py-1"
+                    viewBox="0 0 16 16" style="background-color: rgb(188, 227, 226);">
+                    <path
+                        d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5" />
+                </svg>
+                <h3>Customer Support</h3>
+                <p>Feel free to leave a comment about our service.</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="border text-center py-5" style="background-color: rgb(188, 227, 226); border-radius: 10px;">
+        <h5>WHO WE ARE</h5>
+        <h1>Professional Doctor With Years Of Experience</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis,
+            pulvinar dapibus leo. Lorem ipsum dolor sit amet.</p>
+    </div>
+    <br>
+    <div class="team text-center">
+        <h5>TEAM</h5>
+        <h1>Meet Our Professionals</h1>
+        <p>Some of the best Doctors in town all working in the same clinic having the same goal.</p>
+    </div>
+
+    <div class="row">
+        <div class="col-3 text-center">
+            <img width="298" height="394"
+                src="https://template-kit.rootlayers.com/gomedic/wp-content/uploads/sites/29/2021/08/team6.png"
+                class="attachment-large size-large" alt="" loading="lazy">
+            <p class="fw-bold">dr. Harman White</p>
+            <p>CEO & Director</p>
+        </div>
+
+        <div class="col-3 text-center">
+            <img width="298" height="394"
+                src="https://template-kit.rootlayers.com/gomedic/wp-content/uploads/sites/29/2021/08/team1.png"
+                class="attachment-large size-large" alt="" loading="lazy">
+            <p class="fw-bold">dr. Harman White</p>
+            <p>CEO & Director</p>
+        </div>
+
+        <div class="col-3 text-center">
+            <img width="298" height="394"
+                src="https://template-kit.rootlayers.com/gomedic/wp-content/uploads/sites/29/2021/08/team3.png"
+                class="attachment-large size-large" alt="" loading="lazy">
+            <p class="fw-bold">dr. Harman White</p>
+            <p>CEO & Director</p>
+        </div>
+
+        <div class="col-3 text-center">
+            <img width="298" height="394"
+                src="https://template-kit.rootlayers.com/gomedic/wp-content/uploads/sites/29/2021/08/team4.png"
+                class="attachment-large size-large" alt="" loading="lazy">
+            <p class="fw-bold">dr. Harman White</p>
+            <p>CEO & Director</p>
+        </div>
+    </div>
+    <br>
+    <div class="container text-center border py-5 px-5"
+        style="border-radius: 10px; background-color: rgb(188, 227, 226);">
+        <h1>Get Your Free Medical Checkup. Let's Connect With Us!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <button type="button" class="btn btn-primary">Contact US</button>
+    </div>
+</body>
+
+</html>
