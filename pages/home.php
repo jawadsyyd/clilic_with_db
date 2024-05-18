98403b3415421ebe90711d002be29297c68c3be0
BilalMaryam <142595592+BilalMaryam@users.noreply.github.com>
2024-05-18 20:42:13 +0300

    adding the appointment form

--- pages/home.php
+++ pages/home.php
@@ -132,9 +132,30 @@
     <br>
     <div class="container text-center border py-5 px-5"
         style="border-radius: 10px; background-color: rgb(188, 227, 226);">
-        <h1>Get Your Free Medical Checkup. Let's Connect With Us!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-        <button type="button" class="btn btn-primary">Contact US</button>
+        <h1>Get Your Free Medical Checkup. Book an Appointment!</h1>
+        <form>
+            <div class="container mt-4">
+                <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" id="name" class="form-control" placeholder="Enter patient's name" required>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="date" class="form-label">Date:</label>
+                    <input type="date" id="date" class="form-control" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="disease" class="form-label">Disease:</label>
+                    <textarea rows="5" type="text" id="disease" class="form-control" placeholder="Enter patient's disease description" required></textarea>
+                </div>
+                </div>
+            </div>
+        </form>
+
+
+        <button type="button" class="btn btn-primary">Book an Appointment</button>
     </div>
 </body>
 
